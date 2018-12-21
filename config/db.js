a77cc6f8cd08ae8207822790d5256e1da339e682
Madieng <mbegue.m@gmail.com>
2018-12-21 22:56:13 +0100

    dossier back

--- config/db.js
+++
@@ -1,3 +0,0 @@
-let config = require('./config')
-let mongoose = require('mongoose')
-mongoose.connect('mongodb://' + config.db.localhost + '/' + config.db.database, { useNewUrlParser: config.db.useNewUrlParser })
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-let router = require('express').Router()
-
-router.get('/', (req, res) => {
-    res.json({'message': 'Root Api v1'})
-})
-
-module.exports = router
